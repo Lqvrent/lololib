5b2a887a221db71cb77ce6007c674ea3d0b0cfbc
Lqvrent <71844534+Lqvrent@users.noreply.github.com>
2022-10-27 21:15:42 +0000

    Deploying to main from  @ 0000af4e53845b9576213c9d4510f45165a25f0a 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "eis",
+  1: "eis",
+  2: "es"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions"
+};
+
