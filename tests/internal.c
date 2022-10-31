56fc51dda22d8a4f26cb5fd07ae0d36d6f3d2fbc
Lqvrent <laurent.amat@epitech.eu>
2022-10-31 23:45:45 +0100

    fix(unit_tests): fixed build, broken by internal files

--- tests/internal.c
+++ tests/internal.c
@@ -1,23 +1,23 @@
 #include <criterion/criterion.h>
 #include "../includes/lolo/internal.h"
 
-Test(internal, current_errorno_get)
+Test(internal, __current_errorno_get)
 {
-    cr_assert_eq(current_errorno(-1), 0);
+    cr_assert_eq(__current_errorno(-1), 0);
 }
 
-Test(internal, current_errorno_set)
+Test(internal, __current_errorno_set)
 {
-    cr_assert_eq(current_errorno(0), 0);
+    cr_assert_eq(__current_errorno(0), 0);
 }
 
-Test(internal, current_errorno_set_max)
+Test(internal, __current_errorno_set_max)
 {
-    cr_assert_eq(current_errorno(ERRORS_COUNT - 1), ERRORS_COUNT - 1);
+    cr_assert_eq(__current_errorno(__ERRORS_COUNT - 1), __ERRORS_COUNT - 1);
 }
 
-Test(internal, current_errorno_set_over)
+Test(internal, __current_errorno_set_over)
 {
-    current_errorno(0);
-    cr_assert_eq(current_errorno(ERRORS_COUNT), 0);
+    __current_errorno(0);
+    cr_assert_eq(__current_errorno(__ERRORS_COUNT), 0);
 }
