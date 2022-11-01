b92fda8ffb6cd725b7f08de1d6448cd1858805a6
Lqvrent <laurent.amat@epitech.eu>
2022-11-01 01:34:23 +0100

    feat(queue): added source code

---
+++ sources/queue.c
@@ -0,0 +1,94 @@
+#include "lolo/queue.h"
+#include <stdlib.h>
+
+void queue_enqueue(queue_t **queue, void *data)
+{
+    queue_node_t *node = NULL;
+
+    if (*queue == NULL) {
+        *queue = malloc(sizeof(queue_t));
+        (*queue)->data = NULL;
+        (*queue)->head = NULL;
+        (*queue)->tail = NULL;
+    }
+    node = malloc(sizeof(queue_node_t));
+    node->data = data;
+    node->next = NULL;
+    if ((*queue)->data == NULL) {
+        (*queue)->data = node;
+        (*queue)->head = node;
+        (*queue)->tail = node;
+    } else {
+        (*queue)->tail->next = node;
+        (*queue)->tail = node;
+    }
+}
+
+void *queue_dequeue(queue_t **queue)
+{
+    queue_node_t *node = NULL;
+    void *data = NULL;
+
+    if (queue == NULL || *queue == NULL || (*queue)->data == NULL)
+        return (NULL);
+    node = (*queue)->data;
+    data = node->data;
+    (*queue)->data = node->next;
+    if ((*queue)->data == NULL) {
+        (*queue)->head = NULL;
+        (*queue)->tail = NULL;
+    }
+    free(node);
+    return data;
+}
+
+void *queue_peek(queue_t *queue)
+{
+    if (queue == NULL || queue->data == NULL)
+        return (NULL);
+    return (queue->data->data);
+}
+
+unsigned int queue_size(queue_t *queue)
+{
+    unsigned int size = 0;
+    queue_node_t *tmp = NULL;
+
+    if (queue == NULL || queue->data == NULL)
+        return (0);
+    tmp = queue->data;
+    while (tmp != NULL) {
+        size++;
+        tmp = tmp->next;
+    }
+    return (size);
+}
+
+void queue_free(queue_t *queue)
+{
+    queue_node_t *tmp = NULL;
+
+    if (queue == NULL)
+        return;
+    while (queue->data != NULL) {
+        tmp = queue->data;
+        queue->data = queue->data->next;
+        free(tmp);
+    }
+    free(queue);
+}
+
+void queue_free_all(queue_t *queue)
+{
+    queue_node_t *tmp = NULL;
+
+    if (queue == NULL)
+        return;
+    while (queue->data != NULL) {
+        tmp = queue->data;
+        queue->data = queue->data->next;
+        free(tmp->data);
+        free(tmp);
+    }
+    free(queue);
+}
